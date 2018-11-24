4e98d90f0a0364227170e0513018ea76ecb4dc0e
Alex Garrett <agarrettR8@gmail.com>
2018-11-24 00:12:20 +0000

    added some comments

--- static/blog/js/posts.js
+++ static/blog/js/posts.js
@@ -1,7 +1,10 @@
 let codeBlocks = document.querySelectorAll('code');
 
+// Finds all instances of '&amp;'
 const re = /&amp;/g;
 
+/* Used to undo the double-escaping of ampersands in code blocks that the
+mardownify function unfortunately introduces. */
 for (let block of codeBlocks) {
   block.innerHTML = block.innerHTML.replace(re, '&');
 }
