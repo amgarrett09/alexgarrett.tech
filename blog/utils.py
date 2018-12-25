d2c46e0a70b781fe3ad660356b2299cf0b62d6f1
Alex Garrett <agarrettR8@gmail.com>
2018-12-25 15:41:44 +0000

    fixed some bugs in syntax highlighting

--- blog/utils.py
+++ blog/utils.py
@@ -6,13 +6,16 @@ import bleach
 
 RE_FUNCTION = re.compile(r'\b([a-z]+[a-zA-Z_]*)(?=\()', re.M)
 RE_COMMENT = re.compile(r'((?<!\S)(?:\/\/|#).+)', re.M)
+RE_STRING_SINGLE = re.compile(r'(?:&#39;).*?(?:&#39;)', re.M)
+RE_STRING_DOUBLE = re.compile(r'(?:&quot;).*?(?:quot;)', re.M)
+
 KEYWORDS = [
     'def ', 'function ', 'fn ', '=&gt;', 'import ', 'from ', 'let ', 'mut ',
     'const ', 'var ', 'lambda ', 'pub ', 'use ',
 ]
 CONTROL_WORDS = [
     'return ', 'if ', 'elif ', 'else ', 'else if',  'while ',
-    'for ', 'loop', 'await ', 'try '
+    'for ', 'loop', 'await ', 'try ', 'in '
 ]
 
 def highlight_text(st):
@@ -20,9 +23,14 @@ def highlight_text(st):
     code_blocks = tree.findall(".//pre/code")
     for block in code_blocks:
         output = block.text
+        output = output.replace("{}", "&#123;&#125;")
 
         # Highlight comments
         output = highlight_with_regex(output, RE_COMMENT, 'comment')
+
+        # Highlight strings
+        output = highlight_with_regex(output, RE_STRING_SINGLE, 'string')
+        output = highlight_with_regex(output, RE_STRING_DOUBLE, 'string')
 
         # Highlight functions
         output = highlight_with_regex(output, RE_FUNCTION, 'function')
@@ -32,7 +40,7 @@ def highlight_text(st):
             new = '<span class="code-keyword">' + word + '</span>'
             output = output.replace(word, new)
 
-        # Highlight control flow words (like 'if' or 'for)
+        # Highlight control flow words (like 'if' or 'for')
         for word in CONTROL_WORDS:
             new = '<span class="code-control">' + word + '</span>'
             output = output.replace(word, new)
