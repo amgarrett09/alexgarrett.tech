b39d0feff185040c4c8360eabd8f460ac088451d
Alex Garrett <agarrettR8@gmail.com>
2019-01-06 07:08:15 +0000

    site redesign

--- static/blog/js/posts.js
+++ static/blog/js/posts.js
@@ -8,11 +8,6 @@ for (let block of codeBlocks) {
     block.innerHTML = block.innerHTML.replace(re_amps, '&');
 }
 
-let images = document.querySelectorAll('img');
-
-for (let img of images) {
-    img.classList = "img-fluid";
-}
 
 /* Unhide everything once above scripts complete */
 let hiddenElements = document.querySelectorAll('.start-hidden');
