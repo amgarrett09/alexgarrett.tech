fca3ee8c5cea628fd56952c7477a4e7fe967cde5
Alex Garrett <agarrettR8@gmail.com>
2019-01-11 02:46:17 +0000

    added popups to display additional info about projects

---
+++ static/blog/js/home.js
@@ -0,0 +1,26 @@
+const popupDict = {"django-blog-button": "django-blog-popup"};
+
+const buttons = document.querySelectorAll(".projectdetail");
+
+const closelinks = document.querySelectorAll(".close-popup");
+
+// Setting up project buttons to display popups
+buttons.forEach((button) => {
+    button.addEventListener("click", function() {
+        const buttonId = this.id;
+        const popupId = popupDict[buttonId];
+        const popup = document.getElementById(popupId);
+        popup.classList.toggle("pop-disabled")
+        popup.classList.toggle("active-popup");
+    });
+});
+
+// Setting up links to close popups
+closelinks.forEach((link) => {
+    link.addEventListener("click", function(event) {
+        event.preventDefault();
+        const activePopup = document.querySelector(".active-popup");
+        activePopup.classList.toggle("active-popup");
+        activePopup.classList.toggle("pop-disabled");
+    })
+})
