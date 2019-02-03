b3bc2fa658ae5ee157b5c0a7527fd27d9eb045c1
Alex Garrett <agarrettR8@gmail.com>
2019-02-03 17:24:01 -0500

    testing

--- src/js/posts.js
+++ src/js/posts.js
@@ -20,3 +20,14 @@ codeBlocks.forEach(block => {
     const lang = block.getAttribute("lang");
     block.innerHTML = highlightSyntax(block.innerHTML, lang);
 });
+
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if (new Date().getTime() - start > milliseconds) {
+            break;
+        }
+    }
+}
+
+sleep(10000);
