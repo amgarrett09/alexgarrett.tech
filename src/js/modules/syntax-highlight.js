a806c25bdf561c19676f70f67b98c393e6e8c3e8
Alex Garrett <agarrettR8@gmail.com>
2019-02-09 10:01:31 -0500

    improved python syntax highlighting

--- src/js/modules/syntax-highlight.js
+++ src/js/modules/syntax-highlight.js
@@ -4,7 +4,8 @@ const pyStringSingle = /((?:\').*?(?:\'))/g;
 const pyStringDouble = /((?:\").*?(?:\"))/g;
 const pyFunction = /\b([a-z_]+[a-zA-Z_]*)(?=\()/g;
 const pyKeywords = /\b(def |lambda |in |import |from )/g;
-const pyControl = /\b(return |if |elif |else |while |for |try: |except: )/g;
+const pyControl = /\b(return |if |elif |else |while |for |try:|except | except:)/g;
+const pyObject = /\b([A-Z]+[a-zA-Z]+)(?=(?:\(|\.|:|\n))/g;
 
 /* Pairs of regexes and a string to be used in a span class name. Unfortunately the order here
 matters because of quirks in how innerHTML works. Double-quote strings must be first */
@@ -14,7 +15,8 @@ const pyRegexArray = [
     [pyStringSingle, "string"],
     [pyFunction, "function"],
     [pyKeywords, "keyword"],
-    [pyControl, "control"]
+    [pyControl, "control"],
+    [pyObject, "object"]
 ];
 
 const langDict = { python: pyRegexArray };
@@ -25,6 +27,7 @@ function highlightSyntax(st, lang) {
     }
 
     let result = st;
+    // find the correct regex array, and match-and-replace elements for each regex
     langDict[lang].forEach(pair => {
         result = result.replace(
             pair[0],
