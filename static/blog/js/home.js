a42cf76affa1b65e2320d31a44d0548734e7ac0e
Alex Garrett <agarrettR8@gmail.com>
2019-03-02 13:52:41 -0500

    added new project to home page

--- static/blog/js/home.js
+++ static/blog/js/home.js
@@ -1,43 +1,47 @@
-const popupDict = {"django-blog-button": "django-blog-popup"};
+const popupDict = {
+  "django-blog-button": "django-blog-popup",
+  "audible-sights-button": "audible-sights-popup"
+};
 
 const projectButtons = document.querySelectorAll(".projectdetail");
 
 const closelinks = document.querySelectorAll(".close-popup");
 
 // Setting up buttons to display detail about projects
-projectButtons.forEach((button) => {
-    button.addEventListener("click", function() {
-        const popupId = popupDict[this.id];
-        const popup = document.getElementById(popupId);
-        popup.classList.toggle("pop-disabled");
-        popup.classList.toggle("active-popup");
-        popup.setAttribute("aria-hidden", "false");
-    });
+projectButtons.forEach(button => {
+  button.addEventListener("click", function() {
+    const popupId = popupDict[this.id];
+    const popup = document.getElementById(popupId);
+    popup.classList.toggle("pop-disabled");
+    popup.classList.toggle("active-popup");
+    popup.setAttribute("aria-hidden", "false");
+  });
 });
 
 // Setting up links to close popups
-closelinks.forEach((link) => {
-    link.addEventListener("click", function(event) {
-        event.preventDefault();
-        const activePopup = document.querySelector(".active-popup");
-        activePopup.classList.toggle("active-popup");
-        activePopup.classList.toggle("pop-disabled");
-        popup.setAttribute("aria-hidden", "true");
-    })
-})
+closelinks.forEach(link => {
+  link.addEventListener("click", function(event) {
+    event.preventDefault();
+    const activePopup = document.querySelector(".active-popup");
+    activePopup.classList.toggle("active-popup");
+    activePopup.classList.toggle("pop-disabled");
+    activePopup.setAttribute("aria-hidden", "true");
+  });
+});
 
 // Smooth scrolling
 const links = document.querySelectorAll("a");
 links.forEach(link => {
-    if (link.hash) {
-        link.addEventListener("click", function(event) {
-            event.preventDefault();
-            document.querySelector(this.hash)
-                .scrollIntoView({behavior: 'smooth'});
-        });
-    }
+  if (link.hash) {
+    link.addEventListener("click", function(event) {
+      event.preventDefault();
+      document.querySelector(this.hash).scrollIntoView({ behavior: "smooth" });
+    });
+  }
 });
 
 // Setting focus to main content when user clicks "skip to main content"
-const skipLink = document.querySelector(".skip-to-content")
-skipLink.addEventListener("click", () => document.querySelector(".learnmore").focus())
+const skipLink = document.querySelector(".skip-to-content");
+skipLink.addEventListener("click", () =>
+  document.querySelector(".learnmore").focus()
+);
