30098f77869b4dcc3936cdcc9aaf188d8570c029
Alex Garrett <agarrettR8@gmail.com>
2019-03-09 21:15:50 -0500

    switch to highlight.js for syntax highlighting

--- src/js/posts.js
+++ src/js/posts.js
@@ -1,5 +1,3 @@
-const { highlightSyntax } = require("./modules/syntax-highlight.js");
-
 // Finds all instances of '&amp;'
 const re_amps = /&amp;/g;
 
@@ -9,9 +7,4 @@ document
         block => (block.innerHTML = block.innerHTML.replace(re_amps, "&"))
     );
 
-/* syntax highlighting */
-const codeBlocks = document.querySelectorAll("pre > code");
-codeBlocks.forEach(block => {
-    const lang = "python";
-    block.innerHTML = highlightSyntax(block.innerHTML, lang);
-});
+
